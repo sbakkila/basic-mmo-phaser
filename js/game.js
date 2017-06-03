2750127b5e33f6e0705cd5a2808687316123ffad
Sam Bakkila <sbakkila@gmail.com>
2017-06-03 10:38:06 -0400

    change sprites to water and drones

--- js/game.js
+++ js/game.js
@@ -1,5 +1,5 @@
 /**
- * Created by Jerome on 03-03-16.
+ * Phaser Template Created by Jerome on 03-03-16.
  */
 /*
  * Author: Jerome Renaux
@@ -13,23 +13,27 @@ Game.init = function(){
 };
 
 Game.preload = function() {
-    game.load.tilemap('map', 'assets/map/example_map.json', null, Phaser.Tilemap.TILED_JSON);
-    game.load.spritesheet('tileset', 'assets/map/tilesheet.png',32,32);
-    game.load.image('sprite','assets/sprites/sprite.png');
+    // game.load.tilemap('map', 'assets/map/example_map.json', null, Phaser.Tilemap.TILED_JSON);
+    // game.load.spritesheet('tileset', 'assets/map/tilesheet.png',32,32);
+    game.load.image('backdrop', 'assets/map/ocean.jpg');
+    game.load.image('sprite','assets/sprites/drone2.png');
 };
 
 Game.create = function(){
     Game.playerMap = {};
     var testKey = game.input.keyboard.addKey(Phaser.Keyboard.ENTER);
     testKey.onDown.add(Client.sendTest, this);
-    var map = game.add.tilemap('map');
-    map.addTilesetImage('tilesheet', 'tileset'); // tilesheet is the key of the tileset in map's JSON file
-    var layer;
-    for(var i = 0; i < map.layers.length; i++) {
-        layer = map.createLayer(i);
-    }
-    layer.inputEnabled = true; // Allows clicking on the map ; it's enough to do it on the last layer
-    layer.events.onInputUp.add(Game.getCoordinates, this);
+    // var map = game.add.tilemap('map');
+    // map.addTilesetImage('tilesheet', 'tileset'); // tilesheet is the key of the tileset in map's JSON file
+    // var layer;
+    // for(var i = 0; i < map.layers.length; i++) {
+    //     layer = map.createLayer(i);
+    // }
+    var backdrop = game.add.sprite(0, 0, 'backdrop');
+    backdrop.height = game.height;
+    backdrop.width = game.width;
+    // layer.inputEnabled = true; // Allows clicking on the map ; it's enough to do it on the last layer
+    // layer.events.onInputUp.add(Game.getCoordinates, this);
     Client.askNewPlayer();
 };
 
